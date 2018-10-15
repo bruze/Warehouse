240356d927d43fbcecfe33cb7d5e241a5839adae
Bruno <bruno.garelli@yahoo.com>
2018-10-15 18:25:27 -0300

    Kick-Off

---
+++ Warehouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Warehouse
+//
+//  Created by Bruno Garelli on 10/15/18.
+//  Copyright © 2018 Bruno Garelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
